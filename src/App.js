e879a9f90434ace35145e34385631d2754922193
Alex Smotritsky <alex.smotritsky@gmail.com>
2021-12-23 11:51:00 -0500

    some text changes and conditional display

--- src/App.js
+++ src/App.js
@@ -21,18 +21,18 @@ const App = () => {
     <div>
       <form onSubmit={handleSubmit((data) => handleOnSubmit(data))}>
         <button type="button" onClick={() => {append({ value: "", type: "input" })}}>
-          Add new book
+          Add text input
         </button>
-        <button type="submit">Buy</button>
+        <button type="submit">Submit</button>
         <ul>
           {fields.map((item, index) => (
             <li key={item.id}>
-              {item.type == "input" && <Controller
+              {item.type == "input" ? <Controller
                 name={`books.${index}.value`}
                 control={control}
                 defaultValue={item.value}
                 render={({ field }) => <input {...field} />}
-              />}
+              /> : 'test'}
               <button onClick={() => remove(index)}>Delete</button>
             </li>
           ))}
