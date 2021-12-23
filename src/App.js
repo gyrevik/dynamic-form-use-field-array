c64a4a8eedb1c6bbac8bb2527f95c5cb78303019
Alex Smotritsky <alex.smotritsky@gmail.com>
2021-12-23 11:44:11 -0500

    starting conditional control of display

--- src/App.js
+++ src/App.js
@@ -10,33 +10,33 @@ const App = () => {
 
   const handleOnSubmit = (data) => {
     const { books } = data;
-    const filteredArr = books.map(({ value }) => {
-      return value;
+    const filteredArr = books.map(({ value, type }) => {
+      return {value, type};
     });
 
-    console.log(filteredArr);
+    console.log('filteredArr:', filteredArr);
   };
 
   return (
     <div>
       <form onSubmit={handleSubmit((data) => handleOnSubmit(data))}>
+        <button type="button" onClick={() => {append({ value: "", type: "input" })}}>
+          Add new book
+        </button>
+        <button type="submit">Buy</button>
         <ul>
           {fields.map((item, index) => (
             <li key={item.id}>
-              <Controller
+              {item.type == "input" && <Controller
                 name={`books.${index}.value`}
                 control={control}
                 defaultValue={item.value}
                 render={({ field }) => <input {...field} />}
-              />
+              />}
               <button onClick={() => remove(index)}>Delete</button>
             </li>
           ))}
         </ul>
-        <button type="button" onClick={() => append({ value: "" })}>
-          Add new book
-        </button>
-        <button type="submit">Buy</button>
       </form>
     </div>
   );
