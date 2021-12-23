b06af0a7ccf94e8076c7559ffc97ba766ae13ac9
Alex Smotritsky <alex.smotritsky@gmail.com>
2021-12-23 18:11:28 -0500

    ready to do date conversion

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import ReactDatePicker from "react-datepicker";
-import { TextField } from "@material-ui/core";
 import { useForm, useFieldArray, Controller } from "react-hook-form";
 import "react-datepicker/dist/react-datepicker.css";
 
@@ -11,35 +10,61 @@ const App = () => {
     name: "controls"
   });
 
+  const [firstLoad, setFirstLoad] = useState(false);
+  const [myFields, setMyFields] = useState([]);
+
+  useEffect(() => {
+    let fieldsFromStorage = localStorage.getItem('filteredArr');
+    if (fieldsFromStorage && fieldsFromStorage.length > 0) {
+      fieldsFromStorage = JSON.parse(fieldsFromStorage);
+      if (fieldsFromStorage[0].value !== "") {
+        const date = fieldsFromStorage[0].value;
+        console.log('date:', date)
+      }
+      setMyFields(fieldsFromStorage);
+    }
+    console.log('fieldsFromStorage:', fieldsFromStorage);
+    //setMyFields([]);
+    setFirstLoad(false);
+  }, [firstLoad]);
+
   const handleOnSubmit = (data) => {
+    console.log('data:', data)
     const { controls } = data;
-    const filteredArr = controls.map(({ value, type }) => {
-      return {value, type};
+    const filteredArr = controls.map(({ value, type }, index) => {
+      return {id:index, value, type};
     });
 
     console.log('filteredArr:', filteredArr);
+    localStorage.setItem('filteredArr', JSON.stringify(filteredArr));
   };
+
+  console.log('fields:', fields)
 
   return (
     <div>
       <form onSubmit={handleSubmit((data) => handleOnSubmit(data))}>
-        <button type="button" onClick={() => {append({ value: "", type: "input" })}}>
+        <button type="button" onClick={() => {
+          append({ value: "", type: "input" })
+        }}>
           Add text box
         </button>
-        <button type="button" onClick={() => {append({ value: "", type: "react-datepicker" })}}>
+        <button type="button" onClick={() => {
+          append({ value: "", type: "react-datepicker" })
+        }}>
           Add react-datepicker
         </button>
           
         {fields.map((item, index) => (
           <div key={item.id}>
-            {item.type == "input" && <Controller
+            {item.type === "input" && <Controller
               name={`controls.${index}.value`}
               control={control}
               defaultValue={item.value}
               render={({ field }) => <input {...field} />}
             />}
 
-            {item.type == "react-datepicker" && <Controller
+            {item.type === "react-datepicker" && <Controller
               control={control}
               name={`controls.${index}.value`}
               render={({ field: { onChange, onBlur, value, ref } }) => (
