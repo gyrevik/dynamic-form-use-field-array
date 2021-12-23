05394c15cfd883e4f56f8e859312ef5babddcbfe
Alex Smotritsky <alex.smotritsky@gmail.com>
2021-12-23 12:36:18 -0500

    added react-datepicker

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
 import React from "react";
+import ReactDatePicker from "react-datepicker";
+import { TextField } from "@material-ui/core";
 import { useForm, useFieldArray, Controller } from "react-hook-form";
+import "react-datepicker/dist/react-datepicker.css";
 
 const App = () => {
   const { control, handleSubmit } = useForm();
   const { fields, append, remove } = useFieldArray({
     control,
-    name: "books"
+    name: "controls"
   });
 
   const handleOnSubmit = (data) => {
-    const { books } = data;
-    const filteredArr = books.map(({ value, type }) => {
+    const { controls } = data;
+    const filteredArr = controls.map(({ value, type }) => {
       return {value, type};
     });
 
@@ -21,24 +24,32 @@ const App = () => {
     <div>
       <form onSubmit={handleSubmit((data) => handleOnSubmit(data))}>
         <button type="button" onClick={() => {append({ value: "", type: "input" })}}>
-          Add text input
+          Add text box
+        </button>
+        <button type="button" onClick={() => {append({ value: "", type: "react-datepicker" })}}>
+          Add react-datepicker
         </button>
         <button type="submit">Submit</button>
         <ul>
           {fields.map((item, index) => (
             <li key={item.id}>
               {item.type == "input" && <Controller
-                name={`books.${index}.value`}
+                name={`controls.${index}.value`}
                 control={control}
                 defaultValue={item.value}
                 render={({ field }) => <input {...field} />}
               />}
 
-              {item.type == "radio" && <Controller
-                name={`books.${index}.value`}
+              {item.type == "react-datepicker" && <Controller
                 control={control}
-                defaultValue={item.value}
-                render={({ field }) => <input {...field} />}
+                name={`controls.${index}.value`}
+                render={({ field: { onChange, onBlur, value, ref } }) => (
+                  <ReactDatePicker
+                    onChange={onChange}
+                    onBlur={onBlur}
+                    selected={value}
+                  />
+                )}
               />}
               <button onClick={() => remove(index)}>Delete</button>
             </li>
